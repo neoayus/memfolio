1f032c2cb5613f5dbc30441a154f3d0edabb07d9
neoayus <ayus43@outlook.com>
2026-05-26 11:35:36 +0530

    feat: new component for about

---
+++ src/components/About.jsx
@@ -0,0 +1,27 @@
+export default function About() {
+  return (
+    <article>
+      <div class="text">
+        <h3>Behind the Code</h3>
+        <p class="blackbox">
+          Hey! I'm Ayush, a sophomore Computer Science major. Currently diving
+          deep into the MERN stack, I’m always exploring new ways to bring ideas
+          to life through code. Whether it's customizing my workflow with Neovim
+          or creating responsive web applications, I strive to learn, improve,
+          and innovate with every project. This space is a reflection of my
+          journey in tech, my projects, and the things that inspire me every
+          day. Let’s build something amazing together!
+        </p>
+        <h4>My favourite Technologies includes:</h4>
+        <ul>
+          <li>NVIM</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JS</li>
+          <li>Java</li>
+        </ul>
+      </div>
+      <img src="./assets/images/projects/my-pfp.png" />
+    </article>
+  );
+}
